62c5774ddabc5a9dad6c8c92e51b2c32cf65a2df
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-01-16 19:50:01 +0100

    Updated info.rkt

--- info.rkt
+++ info.rkt
@@ -1,9 +1,9 @@
 #lang setup/infotab
-(define name "pdf-render")
+(define name "racket-poppler")
 (define blurb
-  '("This library lets you render PDF pages to picts and bitmap%s. You can also gather text and layout information. Small bindings for libpoppler."))
+  '("These bindings for libpoppler you render pdf pages to picts and bitmap%s. You can also gather text and layout information. As a bonus render-tex will render TeX into picts."))
 (define primary-file "main.rkt")
 (define categories '(media io))
 (define repositories '("4.x"))
 (define scribblings '(("main.scrbl" ())))
-(define release-notes '((p "Documentation typo/fixes")))
+(define release-notes '((p "Initial release")))
