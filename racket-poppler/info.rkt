511c4216ccd498362a620a5855202e4fff3ece5f
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-04-12 23:51:53 +0200

    Added examples/ to the omit-compile-paths in info.rkt

--- racket-poppler/info.rkt
+++ racket-poppler/info.rkt
@@ -1,9 +1,11 @@
 #lang setup/infotab
 (define name "racket-poppler")
 (define blurb
-  '("These bindings for libpoppler you render pdf pages to picts and bitmap%s. You can also gather text and layout information. As a bonus render-tex will render TeX into picts."))
+  '("Read, render and search pdf files. Pdfs can be rendered to picts and bitmaps. You can also gather text and layout information. As a bonus render-tex will render TeX into picts."))
 (define primary-file "main.rkt")
 (define categories '(media io))
 (define repositories '("4.x"))
 (define scribblings '(("main.scrbl" ())))
 (define release-notes '((p "Initial release")))
+(define omit-compile-paths '("racket-poppler/examples/" "examples/"))
+
