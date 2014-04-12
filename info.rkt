b6b776e4d2e6aa128e26b7483b2c66eb7afca034
Jens Axel Søgaard <jensaxel@soegaard.net>
2014-04-12 21:59:17 +0200

    Make racket-poppler a multi collection. Add precompiled versions of poppler for OS X and Windows

--- info.rkt
+++ info.rkt
@@ -1,9 +1,8 @@
-#lang setup/infotab
-(define name "racket-poppler")
-(define blurb
-  '("These bindings for libpoppler you render pdf pages to picts and bitmap%s. You can also gather text and layout information. As a bonus render-tex will render TeX into picts."))
-(define primary-file "main.rkt")
-(define categories '(media io))
-(define repositories '("4.x"))
-(define scribblings '(("main.scrbl" ())))
-(define release-notes '((p "Initial release")))
+#lang info
+(define collection 'multi)
+(define version    "1")
+(define deps '("base"))
+
+(define build-deps '("at-exp-lib" "rackunit-lib" "scribble-lib"
+                     "racket-doc" "draw-doc" "pict-doc"))
+
---
+++ racket-poppler/info.rkt
@@ -0,0 +1,9 @@
+#lang setup/infotab
+(define name "racket-poppler")
+(define blurb
+  '("These bindings for libpoppler you render pdf pages to picts and bitmap%s. You can also gather text and layout information. As a bonus render-tex will render TeX into picts."))
+(define primary-file "main.rkt")
+(define categories '(media io))
+(define repositories '("4.x"))
+(define scribblings '(("main.scrbl" ())))
+(define release-notes '((p "Initial release")))
