236791b1255dc5fbfde5f7ce8d3c35e584276947
Leif Andersen <leif@leifandersen.net>
2015-02-01 15:21:22 -0500

    Fixed deps and build-deps, raco setup no longer complains
    Does not fix deps for native libraries.

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection 'multi)
 (define version    "1")
-(define deps '("base" "pict"))
+(define deps '("base" "pict" "draw-lib" "slideshow-lib" "web-server-lib"))
 
 (define build-deps '("at-exp-lib" "rackunit-lib" "scribble-lib"
                      "racket-doc" "draw-doc" "pict-doc"))
